4f058b94d5823ff09b2853506615331fb978b491
Justin R. Miller <incanus@codesorcery.net>
2018-09-08 12:14:12 -0700

    cleanups for my use

--- LSM6DS3.py
+++ LSM6DS3.py
@@ -9,16 +9,12 @@ import math
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.I2C as I2C
 
-#Indirizzo fisico del sensore. E' scritto sul sensore stesso
-address = 0x6a
-#Voltaggio d'alimentazione del sensore
-ADC_ref = 5.0
-#Calibrazione del voltaggio a 0g
+ADC_ref = 3.3
+
 zero_x = 1.569
 zero_y = 1.569
 zero_z = 1.569
 
-#La sensibilità è la standard: – 300 mV/g
 sensitivity_x = 0.3
 sensitivity_y = 0.3
 sensitivity_z = 0.3
@@ -34,7 +30,7 @@ class LSM6DS3:
     accel_center_z = 0
 
 
-    def __init__(self, address=0x6a, debug=0, pause=0.8):
+    def __init__(self, address=0x6b, debug=0, pause=0.8):
         self.i2c = I2C.get_i2c_device(address)
         self.address = address
         dataToWrite = 0
@@ -67,8 +63,6 @@ class LSM6DS3:
 
         yv=(value_y/1024.0*ADC_ref-zero_y)/sensitivity_y
         zv=(value_z/1024.0*ADC_ref-zero_z)/sensitivity_z
-        #Calcola l'angolo tra i vettori Y e Z . *57.2957795 è per la conversione
-        #da radianti a gradi. +180 è l'offset
         angle_x =math.atan2(-yv,-zv)*57.2957795+180
 
         return angle_x;
@@ -79,8 +73,6 @@ class LSM6DS3:
 
         xv=(value_x/1024.0*ADC_ref-zero_x)/sensitivity_x
         zv=(value_z/1024.0*ADC_ref-zero_z)/sensitivity_z
-        #Calcola l'angolo tra i vettori X e Z . *57.2957795 è per la conversione
-        #da radianti a gradi. +180 è l'offset
         angle_y =math.atan2(-xv,-zv)*57.2957795+180
 
         return angle_y;
@@ -91,8 +83,6 @@ class LSM6DS3:
 
         xv=(value_x/1024.0*ADC_ref-zero_x)/sensitivity_x
         yv=(value_y/1024.0*ADC_ref-zero_y)/sensitivity_y
-        #Calcola l'angolo tra i vettori Y e X . *57.2957795 è per la conversione
-        #da radianti a gradi. +180 è l'offset
         angle_z =math.atan2(-yv,-xv)*57.2957795+180
 
         return angle_z;
